c07954f4fa1692af803bce69532694112e62a649
Artem <akatrek@gmail.com>
2019-08-12 09:58:08 +0300

    updated

--- btre_project/settings.py
+++ btre_project/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'btre_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'btredb',
+        'USER': 'postgres',
+        'PASSWORD' : 'qwe123qwer',
+        'HOST' : 'localhost',
+
     }
 }
 
@@ -126,3 +130,8 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'btre_project/static')
 ]
+
+
+# Media folder settings
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
